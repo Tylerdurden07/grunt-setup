7c2688b003eedb8da971412afd3ab5b2d3535419
Gnana sekar <gnanasekar@Gnanas-MacBook-Pro.local>
2020-07-17 20:52:24 +0530

    grunt test

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+'use strict';
+var path = require('path');
+var common_resource_path = "../html";
+function absolutePath(file) {
+  return path.join(__dirname, file);
+}
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+        
+            options: {
+              paths: ['html']
+            },
+            // target name
+             src: {
+                 // no need for files, the config below should work
+                 expand: true,
+                 cwd:    "html/less",
+                 src:    ['*.less', '!{boot,var,mix}*.less'],
+                 dest: "html/css",
+                 ext:    ".css"
+             }
+            
+          
+
+
+        // Compile all targeted LESS files individually
+    
+    },
+
+    fonts: {
+        files: ['html/fonts/*'],
+        tasks: ['sync:syncfonts'],
+    },
+
+    jsbeautifier : {
+        files : ['html/css/*.css'
+                ],
+            options : {
+                indent: '  ',
+                openbrace: 'end-of-line',
+                autosemicolon: false
+        }
+    },
+
+    cssmin: {
+        options: {
+            style: 'compressed',
+        },
+        absolute: {
+            files: [{
+                src: ['html/css/*.css'
+                ].map(absolutePath),
+                dest: 'html/css/litc_stylesheet.css'
+                
+            }]
+        },
+    },
+
+  
+   
+
+
+
+ 
+
+
+    });
+
+    //require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+   // grunt.loadNpmTasks("grunt-contrib-jshint");
+    //grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks("grunt-jsbeautifier");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+   // grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask('default', ['less','jsbeautifier','cssmin']);
+};
